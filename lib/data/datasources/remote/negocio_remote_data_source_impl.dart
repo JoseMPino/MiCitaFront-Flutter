109a7fc933a02ed7b240de0ff9565e62a440127c
JoseMPino <j2005mpv@gmail.com>
2025-10-26 17:46:59 -0500

    Jp-ajuste urlPost-negocio

--- lib/data/datasources/remote/negocio_remote_data_source_impl.dart
+++ lib/data/datasources/remote/negocio_remote_data_source_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:mi_cita/core/constants/app_constants.dart';
 import 'package:mi_cita/core/errors/failures.dart';
 import 'package:mi_cita/data/datasources/remote/negocio_remote_data_source.dart';
 import 'package:mi_cita/data/models/negocio/negocio_model.dart';
@@ -11,7 +12,7 @@ class NegocioRemoteDataSourceImpl implements NegocioRemoteDataSource {
   @override
   Future<NegocioModel> crearNegocio(NegocioModel negocio) async {
     try {
-      final response = await dio.post('/negocio', data: negocio.toJson());
+      final response = await dio.post(AppConstants.baseUrl+AppConstants.negocioEndpoint, data: negocio.toJson());
       return NegocioModel.fromJson(response.data);
     } on DioException catch (e) {
       throw ServerException(e.response?.data['message'] ?? 'Error al crear negocio');
